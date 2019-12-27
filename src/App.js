c5a0b8f8c446056d7a2cad89197c09ff95833bb6
clarymt <52682135+clarymt@users.noreply.github.com>
2019-12-27 15:42:46 -0500

    made export links and built out some of card display with some errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+//import Header from "./components/Header";
+import cards from "./cards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cards, 
+    score: 0,
+    highscore: 0
+  };
+  
+  render() {
+    return (
+      //add header within wrapper
+      <Wrapper>
+        {this.state.cards.map(card => (
+          <Card
+            //clickCount to be built here
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
