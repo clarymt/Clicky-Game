84fb56fb52de8e5f23098190a6ce27f772c2fadc
clarymt <52682135+clarymt@users.noreply.github.com>
2019-12-30 15:44:54 -0500

    removed unecessary pseudocode

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,15 @@
-  
+
 import React from "react";
 import "./Header.css";
 
 const Header = props => (
-  
 
-  <nav class="col-xl fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
-    <a className="title" class="navbar-brand" href="#">{props.children}</a>
-    <a className="scores" class="navbar-brand" href="#">Score: {props.score} Highscore: {props.highscore}</a>
-  </nav>
-   
+
+    <nav class="col-xl fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="title" class="navbar-brand" href="#">{props.children}</a>
+        <a className="scores" class="navbar-brand" href="#">Score: {props.score} Highscore: {props.highscore}</a>
+    </nav>
+
 
 );
 
