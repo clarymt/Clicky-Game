97292bd7e80898c446e312a1268feec386fe86a8
clarymt <52682135+clarymt@users.noreply.github.com>
2020-01-12 14:27:12 -0500

    Updates

---
+++ precache-manifest.1c2c464b1623b372e39a355cd37aecaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ad02311e4781d3d38daf163c8f108b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c43b1ce1ae2caa759e3a",
+    "url": "/Clicky-Game/static/css/main.22f9e5ca.chunk.css"
+  },
+  {
+    "revision": "93ae42b0e0bd6d7dd7ad",
+    "url": "/Clicky-Game/static/js/2.1c1c9579.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.1c1c9579.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c43b1ce1ae2caa759e3a",
+    "url": "/Clicky-Game/static/js/main.3f5527a5.chunk.js"
+  },
+  {
+    "revision": "2e174b91be5c95906301",
+    "url": "/Clicky-Game/static/js/runtime-main.59fd1bb4.js"
+  },
+  {
+    "revision": "ce415dbb35bd4ab08e4da63cba604ab9",
+    "url": "/Clicky-Game/static/media/background.ce415dbb.jpg"
+  }
+]);
